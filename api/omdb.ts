2ae497dd78f644a60318af3f6b5977a259ee42e0
nickj <nickjiunchetti@gmail.com>
2023-04-03 13:10:51 -0300

    added vscode configs, and some other files

--- api/omdb.ts
+++
@@ -1,9 +0,0 @@
-export const searchMovies = (text: string) => {
-  return fetch(
-    `http://www.omdbapi.com/?i=tt3896198&s=${text + '&apikey=7c2f1c67'}`
-  )
-    .then((response) => response.json())
-    .then((data) => {
-      return data
-    })
-}
